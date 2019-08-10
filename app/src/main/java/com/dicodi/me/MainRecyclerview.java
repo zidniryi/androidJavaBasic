e6a398b7e217654c27ec6f6365aa173235523956
zidniryi <zidnirydwan@gmail.com>
2019-08-10 21:23:31 +0700

    fixing some erro

--- app/src/main/java/com/dicodi/me/MainRecyclerview.java
+++ app/src/main/java/com/dicodi/me/MainRecyclerview.java
@@ -2,10 +2,14 @@ package com.dicodi.me;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainRecyclerview extends AppCompatActivity {
     private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,15 @@ public class MainRecyclerview extends AppCompatActivity {
 
             rvHeroes = findViewById(R.id.rv_hero);
             rvHeroes.setHasFixedSize(true);
+
+            list.addAll(HeroesData.getListData());
+            showRecyclerList();
         }
+    }
+
+    private void showRecyclerList() {
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
     }
 }
